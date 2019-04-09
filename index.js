4361d2885dff8af742012c18effd60227722da73
J. PANUPONG <panupong.pj15@mail.kmutt.ac.th>
2019-04-10 02:03:24 +0700

    [UPDATE]

--- index.js
+++ index.js
@@ -46,4 +46,42 @@ server()
       });
     });
   })
+  .post("/dialog", function(req, res) {
+    console.log(req);
+    res.json({
+      status: 200,
+      message: "thx"
+    });
+    // "use strict";
+
+    // const functions = require("firebase-functions");
+    // const { WebhookClient } = require("dialogflow-fulfillment");
+    // const { Card, Suggestion } = require("dialogflow-fulfillment");
+
+    // process.env.DEBUG = "dialogflow:debug"; // enables lib debugging statements
+
+    // exports.dialogflowFirebaseFulfillment = functions.https.onRequest(
+    //   (request, response) => {
+    //     const agent = new WebhookClient({ request, response });
+    //     console.log(
+    //       "Dialogflow Request headers: " + JSON.stringify(request.headers)
+    //     );
+    //     console.log("Dialogflow Request body: " + JSON.stringify(request.body));
+
+    //     function welcome(agent) {
+    //       agent.add(`Welcome to my agent!`);
+    //     }
+
+    //     function fallback(agent) {
+    //       agent.add(`I didn't understand`);
+    //       agent.add(`I'm sorry, can you try again?`);
+    //     }
+    //     let intentMap = new Map();
+    //     intentMap.set("Default Welcome Intent", welcome);
+    //     intentMap.set("Default Fallback Intent", fallback);
+
+    //     agent.handleRequest(intentMap);
+    //   }
+    // );
+  })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
